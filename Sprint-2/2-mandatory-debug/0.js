dc520df63e12d661b02ac73ce9ea7fa3a77a6d79
Roman <roman.boxes@gmail.com>
2025-12-02 12:37:09 +0100

    2-mandatory-debug
    1 - replaced console.log in to return
    2 - removed semicolom after return statement and retuned sum of a and b
    3 - added argument for function.
    - replaced num in to a argument inside the function

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,9 +1,10 @@
 // Predict and explain first...
 
 // =============> write your prediction here
-
+// We're going to get 320 and 'The result of multiplying 10 and 32 is undefined' 
+// because the multiply function does not return a value; it only logs the product to the console.
 function multiply(a, b) {
-  console.log(a * b);
+  return (a * b);
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
@@ -12,3 +13,4 @@ console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+// we have to replace console.log with return in the multiply function
--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -2,11 +2,16 @@
 
 // Predict the output of the following code:
 // =============> Write your prediction here
-
+// The output will be:
+// The last digit of 42 is 3
+// The last digit of 105 is 3
+// The last digit of 806 is 3
+// because the function getLastDigit always returns the last digit of the constant variable num which is set to 103.
 const num = 103;
 
-function getLastDigit() {
-  return num.toString().slice(-1);
+function getLastDigit(a) {
+  return a.toString().slice(-1);
+
 }
 
 console.log(`The last digit of 42 is ${getLastDigit(42)}`);
@@ -19,6 +24,7 @@ console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 // =============> write your explanation here
 // Finally, correct the code to fix the problem
 // =============> write your new code here
-
+// now it's working properly because we are returning the last digit of the argument a passed to the function getLastDigit 
+// instead of the constant variable num.
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
