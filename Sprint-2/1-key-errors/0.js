5bf5416aadd23a6bcb4bd7bcbac48827840964de
Roman <roman.boxes@gmail.com>
2025-12-02 11:36:09 +0100

    1-key-errors
    - 0 let this let statement is trying to declare a variable with the same name as the function parameter
    - 1 const decimalNumber = 0.5; and callin the function, for example console.log(convertToPercentage(0.75)); // 75%
    - 2 square(num) {  // changed parameter name from 3 to num. console.log(square(4)); // 16

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,26 @@
 // Predict and explain first...
 //  =============> write your prediction here
 
+// this let statement is trying to declare a variable with the same name as the function parameter 'str',
+// which will cause a syntax error because you cannot redeclare a parameter within the same scope.
+
 // call the function capitalise with a string input
+
+// we don't call the function here, but if we did, it would result in an error due to the redeclaration of 'str'.
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
-}
+// function capitalise(str) {
+//   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//   return str;
+// }
 
 // =============> write your explanation here
 // =============> write your new code here
+
+
+function capitalise(str) {
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
+}
+console.log(capitalise('hello'));
+// Hello 
