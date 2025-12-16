6aa5cce7d2ff9a6f6dad448d25702bef4497f10f
Roman <roman.boxes@gmail.com>
2025-12-16 22:32:10 +0100

    fix(count.js) tests
    - not exist in string
    - case-sensetive
    - single occurrence

--- Sprint-3/3-mandatory-practice/implement/count.test.js
+++ Sprint-3/3-mandatory-practice/implement/count.test.js
@@ -17,6 +17,28 @@ test("should count multiple occurrences of a character", () => {
   expect(count).toEqual(5);
 });
 
+test("should return 0 when character does not exist in string", () => {
+  const str = "Hello World";
+  const char = "x";
+  const count = countChar(str, char);
+  expect(count).toEqual(0);
+});
+
+test("should be case-sensitive when counting characters", () => {
+  const str = "AbcabcABC";
+  const char = "a";
+  const count = countChar(str, char);
+  expect(count).toEqual(1);
+});
+
+test("should count single occurrence of a character", () => {
+  const str = "Hello";
+  const char = "e";
+  const count = countChar(str, char);
+  expect(count).toEqual(1);
+});
+
+
 // Scenario: No Occurrences
 // Given the input string str,
 // And a character char that does not exist within the case-sensitive str,
