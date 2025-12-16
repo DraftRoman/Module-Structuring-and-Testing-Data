f5134d72e91ba62778c6ce63bc6e83ba03665527
Roman <roman.boxes@gmail.com>
2025-12-16 22:06:01 +0100

    3-get-card-value.js exercise 3
    - covered by seven new tests.
    - if logic from previous tast

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -1,5 +1,7 @@
 function getCardValue(card) {
-    // replace with your code from key-implement
-    return 11;
+    if (card.charAt(0) === "A") return 11;
+    else if ((card.charAt(0) === "K") || (card.charAt(0) === "Q") || (card.charAt(0) === "J") || (card.slice(0, 2) === "10")) return 10;
+    else if (parseInt(card.charAt(0)) >= 2 && parseInt(card.charAt(0)) <= 9) return parseInt(card.charAt(0));
+    else throw new Error("Invalid card rank.");
 }
 module.exports = getCardValue;
