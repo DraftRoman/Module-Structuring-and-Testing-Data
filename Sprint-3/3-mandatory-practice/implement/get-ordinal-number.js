3358580f7b176717fea49c796968e0d875c3f594
Roman <roman.boxes@gmail.com>
2025-12-17 19:13:32 +0100

    optimization of the code
    - strict equal sign
    - and less lines of code, first three cases together with the rest of code, and default.

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -10,22 +10,15 @@ function getOrdinalNumber(num) {
     if (num < 0) {
         throw new Error("Invalid input: expected an ordinal number. Negative numbers are not allowed.");
     }
-
     switch (true) {
-        case num === 1:
-            return "1st";
-        case num === 2:
-            return "2nd";
-        case num === 3:
-            return "3rd";
-        case num % 10 == 1 && num % 100 != 11:
+        case num % 10 === 1 && num % 100 !== 11:
             return num + "st";
-        case num % 10 == 2 && num % 100 != 12:
+        case num % 10 === 2 && num % 100 !== 12:
             return num + "nd";
-        case num % 10 == 3 && num % 100 != 13:
+        case num % 10 === 3 && num % 100 !== 13:
             return num + "rd";
-        case num >= 0:
-            return num + "th"
+        default:
+            return num + "th";
     }
 }
 
