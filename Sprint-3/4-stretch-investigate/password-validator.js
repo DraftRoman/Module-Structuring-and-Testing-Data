4b3507becfa40c1c90447f4153024cbeab28ea3b
Roman <roman.boxes@gmail.com>
2025-12-19 21:50:56 +0100

    4-Password validation (5 characters / A-Z / a-z)
    - Have at least 5 characters.
    - Have at least one English uppercase letter (A-Z)
    - Have at least one English lowercase letter (a-z)

--- Sprint-3/4-stretch-investigate/password-validator.js
+++ Sprint-3/4-stretch-investigate/password-validator.js
@@ -1,5 +1,12 @@
 function passwordValidator(password) {
-    return password.length < 5 ? false : true
+    let hasLowercase = false;
+    let hasUppercase = false;
+    for (let i = 0; i < password.length; i++) {
+        const char = password[i];
+        if (char >= "a" && char <= "z") hasLowercase = true;
+        if (char >= "A" && char <= "Z") hasUppercase = true;
+    }
+    return hasLowercase && hasUppercase && password.length >= 5;
 }
 
 
